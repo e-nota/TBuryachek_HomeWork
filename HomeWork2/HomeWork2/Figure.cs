0e2ff1a36f78f5c754263b7e92df2f0fabea2864
e-nota <tat.novak@gmail.com>
2021-07-26 23:36:53 +0300

    HW2
    HomeWork2 - 1st edition

---
+++ HomeWork2/HomeWork2/Figure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    public class Figure
+    {
+        public int NumberOfFaces { get; set; }
+        public double LengthOfFaces { get; set; }
+        public double Area { get; set; }
+    }
+}
---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Класс
+            Console.WriteLine("For class: ");
+            Figure figure = new Figure { NumberOfFaces = 4, LengthOfFaces = 4, Area = 0 };
+            
+            Figure NewAreaFigure = CalcArea1(figure);
+            Console.WriteLine($"figure: NumberOfFaces = {figure.NumberOfFaces}, LengthOfFaces = {figure.LengthOfFaces}, Area = {figure.Area}");
+            Console.WriteLine($"NewAreaFigure: NumberOfFaces = {NewAreaFigure.NumberOfFaces}, LengthOfFaces = {NewAreaFigure.LengthOfFaces}, Area = {NewAreaFigure.Area}");
+
+            CalcArea2(figure);
+            Console.WriteLine($"NumberOfFaces = {figure.NumberOfFaces}, LengthOfFaces = {figure.LengthOfFaces}, Area = {figure.Area}");
+
+            // Структура
+            Console.WriteLine("For structure: ");
+            FigureStruct figurestr = new FigureStruct { NumberOfFaces = 4, LengthOfFaces = 4, Area = 0 };
+
+            FigureStruct NewAreaFigureStr = CalcArea1(figurestr);
+            Console.WriteLine($"figure: NumberOfFaces = {figure.NumberOfFaces}, LengthOfFaces = {figure.LengthOfFaces}, Area = {figure.Area}");
+            Console.WriteLine($"NewAreaFigure: NumberOfFaces = {NewAreaFigure.NumberOfFaces}, LengthOfFaces = {NewAreaFigure.LengthOfFaces}, Area = {NewAreaFigure.Area}");
+
+            CalcArea2(figurestr);
+            Console.WriteLine($"NumberOfFaces = {figure.NumberOfFaces}, LengthOfFaces = {figure.LengthOfFaces}, Area = {figure.Area}");
+
+        }
+        public static Figure CalcArea1 (Figure figure)
+        {
+                        
+            int n = figure.NumberOfFaces;
+            double a = figure.LengthOfFaces;
+            double S = (n * Math.Pow(a, 2)) / (4 * Math.Tan(180 / n)); //?
+
+            figure = new Figure() { NumberOfFaces = 4, LengthOfFaces = 4, Area = S }; //?
+            
+            return figure;
+         }
+
+        public static void CalcArea2 (Figure figure)
+        {
+            int n = figure.NumberOfFaces;
+            double a = figure.LengthOfFaces;
+            double S = (n * Math.Pow(a, 2)) / (4 * Math.Tan(180 / n)); //?
+            figure.Area = S;
+        }
+//---
+        public static FigureStruct CalcArea1(FigureStruct figurestr)
+        {
+
+            int n = figurestr.NumberOfFaces;
+            double a = figurestr.LengthOfFaces;
+            double S = (n * Math.Pow(a, 2)) / (4 * Math.Tan(180 / n)); //?
+
+            figurestr = new FigureStruct() { NumberOfFaces = 4, LengthOfFaces = 4, Area = S }; //?
+
+            return figurestr;
+        }
+
+        public static void CalcArea2(FigureStruct figurestr)
+        {
+            int n = figurestr.NumberOfFaces;
+            double a = figurestr.LengthOfFaces;
+            double S = (n * Math.Pow(a, 2)) / (4 * Math.Tan(180 / n)); //?
+            figurestr.Area = S;
+        }
+    }
+}
