059c0a1cd1bbd27de5af2e8f06a38e9f3b73126c
e-nota <tat.novak@gmail.com>
2021-08-04 07:52:41 +0300

    Update Program.cs

--- HomeWork6/HomeWork6/Program.cs
+++ HomeWork6/HomeWork6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace HomeWork6
 {
@@ -11,10 +12,10 @@ namespace HomeWork6
             Dictionary<Figure, string> DFigures = new Dictionary<Figure, string>();
 
             Random rnd = new Random();
-            int count = 100000;
+            int count = 50000;
             for (int i = 1; i <= count; i++)
             {
-                var newfigure = new Figure() { SideCount = rnd.Next(3, 100), SideLength = rnd.Next(1, 10000) };
+                var newfigure = new Figure() { SideCount = rnd.Next(3, 1000), SideLength = rnd.Next(1, 10000) };
 
                 if (!LFigures.Contains(newfigure))
                 {
@@ -26,6 +27,37 @@ namespace HomeWork6
                     count++;
                 }
             }
+
+            Figure SearchFigure = new Figure();
+            SearchFigure = LFigures[LFigures.Count - 2];
+            Figure result = new Figure();
+
+            // Searching in List
+            var timerfindlist = Stopwatch.StartNew();
+            foreach (var item in LFigures)
+            {
+                if (item.Equals(SearchFigure))
+                {
+                    result = item;
+                    break;
+                }
+            }
+            timerfindlist.Stop();
+            Console.WriteLine($"Figure searching in list takes {timerfindlist.Elapsed} ms");
+
+            // Searching in Dictionary
+            var timerfinddict = Stopwatch.StartNew();
+            foreach (var item in DFigures.Keys)
+            {
+                if (item.Equals(SearchFigure))
+                {
+                    result = item;
+                    break;
+                }
+            }
+            timerfinddict.Stop();
+            Console.WriteLine($"Figure searching in dictionary takes {timerfinddict.Elapsed} ms");
+
         }
     }
 }
