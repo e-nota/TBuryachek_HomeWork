d15fb7f2821f8ce615214635316ff0d886b2e7ce
e-nota <tat.novak@gmail.com>
2021-08-25 07:44:03 +0300

    25.08.2021

---
+++ HomeWork8/HomeWork8.Tests/CalculatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xunit;
+
+namespace HomeWork8.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+         public void GetSum_2_plus_3_eq_5()
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.GetSum(2, 3);
+
+            //Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void GetMultiply_2_To_3_Eq_6()
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.GetMultiply(2, 3);
+
+            //Assert
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void GetDivide_6_To_2_Eq_3()
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.GetDivide(6, 2);
+
+            //Assert
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void GetDivideByZero()
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var result = calculator.GetDivide(1, 0);
+           //Assert
+            Assert.Equal(double.PositiveInfinity, result);
+          
+        }
+    }
+}
---
+++ HomeWork8/HomeWork8/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8
+{
+    public class Calculator
+    {
+        public double GetSum (double a, double b)
+        {
+            return a + b;
+        }
+        public double GetSubstract (double a, double b)
+        {
+            return a - b;
+        }
+
+        public double GetMultiply (double a, double b)
+        {
+            return a * b;
+        }
+        public double GetDivide (double a, double b)
+        {
+            return Math.Round(a / b);
+        }
+
+    }
+}
