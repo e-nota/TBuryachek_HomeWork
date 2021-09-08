9e2d5d15c910898edd1bf73107aa2680da9ea0ec
e-nota <tat.novak@gmail.com>
2021-09-08 08:12:00 +0300

    08.09.2021

---
+++ HomeWork10/HomeWork10/Job.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeWork10
+{
+    public class Job : IJobExecutor
+    {
+        public int Amount { get; set; }
+
+        public int maxConcurrent = 3;
+
+        public CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        public  List<Action> actions = new List<Action>();
+
+        public void Start(int maxConcurrent)
+        {
+            int cnt = 0;
+            List<Action> actionsnew = new List<Action>(actions);
+
+
+            foreach (var action in actions)
+            {
+                if ((cnt < maxConcurrent) && (actions.Count > cnt))
+                {
+                    var task = Task.Factory.StartNew( action, cancellationTokenSource.Token);
+                    Thread.Sleep(1000);
+                    // cancellationTokenSource.Cancel();
+                    //CancellationToken cancellationToken = cancellationTokenSource.Token;
+                    cnt++;
+                    actionsnew.Remove(action);
+                }  
+                actions = actionsnew;
+            }
+        }
+
+        public void Stop()
+        {
+            cancellationTokenSource.Cancel();
+        }
+
+        public void Add(Action action)
+        {
+            Amount = Amount + 1;
+            actions.Add(action);
+        }
+
+        public void Clear()
+        {
+            Stop();
+            actions.Clear();
+        }
+
+    }
+}
---
+++ HomeWork10/HomeWork10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeWork10
+{
+    class Program
+    {
+        public static int num = 1;
+        public static void Main(string[] args)
+        {
+            var job = new Job();
+
+            for (int i = 0; i < 10; i++)
+            {
+                job.Add(DoSomething);
+            }
+
+            job.Start(3);
+            Console.WriteLine("---");
+            job.Start(2);
+            Console.WriteLine("---");
+            
+            job.Stop();
+            job.Start(5);
+            Console.WriteLine("---");
+            job.Clear();
+            job.Start(2);
+            Console.WriteLine("---");
+        }
+
+        public static void  DoSomething()
+        {
+            Console.WriteLine($"Action start {num}");
+            Thread.Sleep(1000);
+            num++;
+
+        }
+    }
+}
